6d0e79c6da3c2564ea2b7dc2ee77562aab86e4b2
krontogiannis <rodoconst@hotmail.com>
2014-05-12 16:13:13 +0300

    Refactoring tests

--- tests/MBrace.Runtime.Tests/Helpers.fs
+++ tests/MBrace.Runtime.Tests/Helpers.fs
@@ -582,3 +582,51 @@
                 with 
                     | :? DivideByZeroException as exn -> return cr.TryValue.IsNone
             }
+
+
+    module PrimesTest = 
+
+        let isPrime (n:int) =
+            let bound = int (System.Math.Sqrt(float n))
+            seq {2 .. bound} |> Seq.exists (fun x -> n % x = 0) |> not
+
+        let primes (fromN, toN) = [| for i = fromN to toN do if isPrime i then yield i |]
+
+        let partitionPairs length n =
+            [| 
+                for i in 0 .. n - 1 ->
+                    let i, j = length * i / n, length * (i + 1) / n in (i + 1, j) 
+            |]
+
+        [<Cloud>]
+        let prepareCloudPrimes n numberOfPartitions = [| for (fromN, toN) in partitionPairs n numberOfPartitions -> cloud { return primes (fromN, toN) }  |]
+
+        [<Cloud>]
+        let parallelPrimes n numberOfPartitions =
+            cloud { 
+                let! values = Cloud.Parallel(prepareCloudPrimes n numberOfPartitions)
+                return values |> Array.concat |> Seq.skip 1 |> Seq.toArray
+            }
+            
+
+//    module Defaults =
+//
+//        let mbracedExe = Path.GetTempPath
+////#if DEBUG
+////            "../../../Nessos.MBrace.Runtime.Daemon/bin/Debug/mbraced.exe" |> Path.GetFullPath
+////#else
+////            "../../../Nessos.MBrace.Runtime.Daemon/bin/Release/mbraced.exe" |> Path.GetFullPath
+////#endif
+//
+//        let setCachingDirectory =
+//            fun () ->
+//                let name = sprintf "nunit.%d" <| System.Diagnostics.Process.GetCurrentProcess().Id
+//                let workingDir = Path.Combine(Path.GetTempPath(), name)
+//                if not <| Directory.Exists workingDir then Directory.CreateDirectory workingDir |> ignore
+//
+//                do 
+//                    Directory.SetCurrentDirectory workingDir
+//                    AssemblyCache.SetCacheDir workingDir
+////                IoC.RegisterValue(Override,workingDir,"AssemblyCachePath")
+//            |> runOnce
+    
