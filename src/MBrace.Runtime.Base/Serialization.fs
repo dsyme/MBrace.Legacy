0323e30c369c20ee9bd96030754e957720ae7419
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-05-13 15:20:39 +0300

    include serialization.fs

---
+++ src/MBrace.Runtime.Base/Serialization.fs
@@ -0,0 +1,45 @@
+﻿namespace Nessos.MBrace.Runtime
+
+    open System.IO
+    open System.Runtime.Serialization
+
+    open Nessos.FsPickler
+
+    open Nessos.Thespian.Serialization
+
+    open Nessos.MBrace.Utils
+
+    type Serialization private () =
+
+        static let lockObj = obj()
+        static let mutable picklerSingleton = None
+
+        static member Register(pickler : FsPickler) =
+            lock lockObj (fun () ->
+                if picklerSingleton.IsSome then
+                    invalidOp "An instance of FsPickler has been registered."
+
+                let actorSerializer = 
+                    new FsPicklerSerializer(pickler) :> IMessageSerializer
+
+                SerializerRegistry.Register(actorSerializer)
+                SerializerRegistry.SetDefault actorSerializer.Name
+                IoC.RegisterValue(pickler, behaviour = OverrideBehaviour.Override)
+
+                picklerSingleton <- Some pickler)
+
+        static member DefaultPickler = 
+            match picklerSingleton with
+            | None -> invalidOp "No instance of FsPickler has been registered."
+            | Some p -> p
+
+
+        static member Serialize (x : 'T) = Serialization.DefaultPickler.Pickle<obj>(x)
+        static member Deserialize<'T> (data : byte []) = Serialization.DefaultPickler.UnPickle<obj>(data) :?> 'T
+
+        static member Clone(x : 'T) =
+            let pickler = Serialization.DefaultPickler
+            use mem = new MemoryStream()
+            pickler.Serialize(mem, x)
+            mem.Position <- 0L
+            pickler.Deserialize<'T>(mem)
