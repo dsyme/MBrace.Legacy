4594d0acad18ebb7f5354f4f2e0413955c1cc1fb
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-05-14 21:10:48 +0300

    add MBrace.Controller

---
+++ src/MBrace.Controller/Config.fs
@@ -0,0 +1,166 @@
+﻿namespace Nessos.MBrace.Runtime.Daemon.Controller
+
+    open System
+    open System.Diagnostics
+    open System.Threading
+    open System.IO
+    open System.Xml
+    open System.Xml.Linq
+
+    open Nessos.UnionArgParser
+
+    open Nessos.MBrace.Client
+    open Nessos.MBrace.Utils
+    open Nessos.MBrace.Utils.Retry
+    open Nessos.MBrace.Utils.String
+
+    type MBraceSession = 
+        { 
+            Id        : System.Guid
+            StartedOn : System.DateTime
+            Nodes     : (Uri * Guid) list // uri * deployment id
+        }
+    with
+        member s.ToXml =
+            let xn = XName.Get
+            let writeNode (uri : Uri, gId : Guid) =
+                XElement(xn "Node", 
+                            XAttribute(xn "Uri", uri), 
+                            XAttribute(xn "DeploymentId", gId))
+            XElement(xn "Session",
+                XElement(xn "Id", s.Id),
+                XElement(xn "StartedOn", s.StartedOn),
+                s.Nodes |> List.map writeNode
+            ).ToString()
+
+        static member OfXml (text : string) =
+            let xn = XName.Get
+            let root = XDocument.Parse(text).Element(xn "Session")
+            let getNode (x : XElement) = 
+                let uri = Uri(x.Attribute(xn "Uri").Value)
+                let gId = x.Attribute(xn "DeploymentId").Value |> Guid.Parse
+                uri, gId
+            {
+                Id        = root.Element(xn "Id").Value |> System.Guid.Parse
+                StartedOn = root.Element(xn "StartedOn").Value |> System.DateTime.Parse
+                Nodes     = root.Elements(xn "Node") |> Seq.map getNode |> Seq.toList
+            }
+
+    type Configuration =
+        | [<NoCommandLine>] Session_File of string
+        | [<NoCommandLine>][<Mandatory>] MBraced_Executable of string
+        | [<First>][<NoAppSettings>][<CustomCommandLine("start")>] Start
+        | [<First>][<NoAppSettings>][<CustomCommandLine("stop")>] Stop
+        | [<First>][<NoAppSettings>][<CustomCommandLine("status")>] Status
+        | [<NoAppSettings>][<AltCommandLine("-n")>] Nodes of int
+        | [<NoAppSettings>][<AltCommandLine("-w")>] Spawn_Window
+        | [<NoAppSettings>][<AltCommandLine("-b")>] Boot
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Session_File _ -> "location of the session file."
+                | MBraced_Executable _ -> "location of the mbraced executable."
+                | Start -> "starts a new mbraced session."
+                | Stop -> "stops the current mbraced session."
+                | Status -> "displays information on the current mbraced session."
+                | Nodes _ -> "specify number of mbraced processes to spawn."
+                | Spawn_Window -> "spawns mbraced processes as popup windows."
+                | Boot -> "boots processes as a standalone local runtime."
+
+
+
+    module internal Implementations =
+
+        let selfExe = System.Reflection.Assembly.GetExecutingAssembly().Location
+
+        let resolveRelativePath (path : string) =
+            if Path.IsPathRooted path then path
+            else Path.GetFullPath <| Path.Combine(Path.GetDirectoryName selfExe, path)
+
+        let defaultSessionFile = Path.Combine(Path.GetTempPath(), "mbrace-session.xml")
+
+        let parseMBracedExecutable (path : string) =
+            let path = resolveRelativePath path
+            if File.Exists path then path
+            else failwith "specified mbraced.exe location does not exist."
+
+        let parseNodes startMode (nodes : int) =
+            if not startMode then failwith "can only specify number of nodes when running in START mode."
+            if nodes <= 1 then failwith "input only valid for nodes > 1."
+            else nodes
+
+        let exiter = new ConsoleProcessExiter(true) :> IExiter
+
+        let argParser = UnionArgParser<Configuration>("USAGE: mbracectl [start|stop|status] ... options")
+
+        let tryParseSessionFile (path : string) =
+            try
+                if File.Exists path then
+                    let text = File.ReadAllText(path)
+                    Some(MBraceSession.OfXml text)
+                else None
+            with e -> exiter.Exit("error: invalid or corrupt session file.", 2)
+
+        let saveSession (path : string) (session : MBraceSession) =
+            try
+                let text = session.ToXml
+                retry (RetryPolicy.Retry(2, 0.5<sec>)) (fun () -> File.WriteAllText(path, text))
+            with e -> 
+                try File.Delete path with _ -> ()
+                exiter.Exit("error: could not update session file.", 2)
+
+        let deleteSessionFile (path : string) =
+            if File.Exists path then
+                try retry (RetryPolicy.Retry(2, 0.5<sec>)) (fun () -> File.Delete path)
+                with e ->
+                    exiter.Exit("error: could not update session file.", 2)
+
+        let tryParseSession (sessionFile : string) =
+            let tryGetNode (uri : Uri, gId : Guid) =
+                try 
+                    let n = new MBraceNode(uri)
+                    if n.DeploymentId <> gId || n.Process.IsNone then None
+                    else Some n
+                with _ -> None
+            
+            match tryParseSessionFile sessionFile with
+            | None -> None
+            | Some session ->
+                match List.parChoose tryGetNode session.Nodes with
+                | [] when session.Nodes.Length > 0 ->
+                    // found a dead session file, delete
+                    deleteSessionFile sessionFile ; None
+                | nodes -> Some (session, nodes)
+
+        let prettyPrint (session : MBraceSession) (nodes : MBraceNode list) =
+            try
+                let title = sprintf "{m}brace session started on %O." session.StartedOn
+                    
+                MBraceNode.PrettyPrint(nodes, header = title, useBorders = false) |> printfn "%s"
+            with e -> exiter.Exit(sprintf "mbracectl: unexpected error: %s" e.Message, 5)
+
+        let spawnNodes spawnWindows (multiNode : (int * bool) option) =
+            try
+                let nodes =
+                    match multiNode with
+                    | None -> [ MBraceNode.Spawn(background = not spawnWindows) ]
+                    // (nodes * boot) option
+                    | Some(n,false) -> MBraceNode.SpawnMultiple(n, background = not spawnWindows)
+                    | Some(n,true) -> 
+                        let r = MBraceRuntime.InitLocal(n, background = not spawnWindows)
+                        r.Nodes
+
+                let session =
+                    { 
+                        Id = Guid.NewGuid() ; StartedOn = DateTime.Now ; 
+                        Nodes = nodes |> List.map (fun n -> n.Uri, n.DeploymentId)
+                    }
+
+                session, nodes
+            with e -> exiter.Exit(sprintf "error initializing session: %s." e.Message, 2)
+
+        let lockSession (sessionFile : string) =
+            match ThreadSafe.tryClaimGlobalMutex <| "mbracectl:" + sessionFile with
+            | None -> exiter.Exit("mbracectl: session appears to be locked by another process.", 2)
+            | Some mtx -> exiter.ExitEvent.Add(fun _ -> mtx.Close())
---
+++ src/MBrace.Controller/Program.fs
@@ -0,0 +1,72 @@
+﻿module Nessos.MBrace.Runtime.Daemon.Controller.MainModule
+
+    open Nessos.MBrace.Utils
+    open Nessos.MBrace.Client
+    open System.Diagnostics
+
+    open Nessos.MBrace.Runtime.Daemon.Controller.Implementations
+
+    [<EntryPoint>]
+    let main _ =
+        
+        if runsOnMono then exiter.Exit("mono not supported... yet!", 1)
+
+        // arg parser
+
+        let results = argParser.Parse(errorHandler = plugExiter exiter)
+
+        let sessionFile = results.GetResult(<@ Session_File @>, defaultSessionFile) |> resolveRelativePath
+        let mbracedExe = results.PostProcessResult(<@ MBraced_Executable @>, parseMBracedExecutable)
+
+        let startMode = results.Contains <@ Start @>
+        let stopMode = results.Contains <@ Stop @>
+        let statusMode = results.Contains <@ Status @>
+
+        if not <| List.exists id [startMode ; stopMode ; statusMode ] then results.Raise("mbracectl: need to specify an operation mode.")
+
+        let nodes = results.TryPostProcessResult(<@ Nodes @>, parseNodes startMode) |> fun n -> defaultArg n 1
+        let spawnWindow = results.Contains <@ Spawn_Window @>
+        let boot = results.Contains <@ Boot @>
+
+        if boot && nodes < 3 then results.Raise("error: need at least 3 nodes to boot.", 2, showUsage = true)
+
+        // init
+        
+        do lockSession sessionFile
+
+        let state = tryParseSession sessionFile
+
+        // parser guarantees that the 3 booleans are mutually exclusive
+        if startMode then
+            let spawnOptions = if nodes = 1 then None else Some(nodes, boot)    
+            let session', nodes =
+                match state with
+                | None -> 
+                    printf "starting {m}brace session... "
+                    MBraceSettings.MBracedExecutablePath <- mbracedExe
+                    let r = spawnNodes spawnWindow spawnOptions
+                    printfn "done"
+                    r
+                | Some _ -> exiter.Exit("mbracectl: an mbrace session is already active.", 0)
+
+            do saveSession sessionFile session'
+            prettyPrint session' nodes
+
+        elif stopMode then
+            match state with
+            | None -> exiter.Exit("mbracectl: no active mbrace session found.")
+            | Some(session, nodes) ->
+                try
+                    printf "stopping {m}brace session... "
+                    // TODO: make cooperative
+                    for n in nodes do n.Kill ()
+                    printfn "done"
+                with e -> exiter.Exit(sprintf "\nmbracectl error: %s" e.Message, id = 3)
+
+        else
+            match state with
+            | None -> printfn "no active {m}brace session."
+            | Some (s, nodes) -> prettyPrint s nodes
+
+        // use exiter to trigger exit events
+        exiter.Exit(id = 0)
