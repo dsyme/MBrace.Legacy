0d69e8532690b1ca7ce67528678b4d42694d965f
krontogiannis <rodoconst@hotmail.com>
2014-05-16 17:42:19 +0300

    MBrace.SqlServer

--- src/MBrace.SqlServer/Test.fsx
+++ src/MBrace.SqlServer/Test.fsx
@@ -1,37 +1,57 @@
-﻿#r "Nessos.MBrace.Utils"
-#r "Nessos.MBrace.Actors"
-#r "Nessos.MBrace.Base"
-#r "Nessos.MBrace.Store"
-#r "Nessos.MBrace.Client"
+﻿//#r "Nessos.MBrace.Utils"
+//#r "Nessos.MBrace.Actors"
+//#r "Nessos.MBrace.Base"
+//#r "Nessos.MBrace.Store"
+//#r "Nessos.MBrace.Client"
 
-#r "System.Data"
-#r "System.Data.Linq"
+//#r "System.Data"
+//#r "System.Data.Linq"
 
-#r "bin/Debug/Nessos.MBrace.Store.SqlServer.dll"
+
+#I "bin/Debug/"
+#r "MBrace.SqlServer.dll"
+#r "MBrace.Utils.dll"
+#r "MBrace.Runtime.Base.dll"
 
 open System
-open System.Data
-open Nessos.MBrace.Store
-open Nessos.MBrace.Client
-open Nessos.MBrace.Utils
-open Nessos.MBrace.Store.SqlServer
+open System.Data.SqlClient
+open Nessos.MBrace.SqlServer
+open Nessos.MBrace.Runtime.Store
 
-let sqlstore = new SqlServerStore("Server=CHURCH;Database=MBrace;User Id=sa;Password=12345678;") :> IStore
+type Async<'T> with
+    member this.Run () = Async.RunSynchronously this
+    
+
+let conn = "Data Source=(localdb)\Projects;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False"
+
+let sqlstore = new SqlServerStore(conn) :> IStore
 
 
-sqlstore.Create("theFolder", "theFile", (fun x -> x.WriteByte(88uy)))
+sqlstore.Create("theFolder", "theFile", (fun x -> async { x.WriteByte(88uy) })).Run()
 
-sqlstore.Read("theFolder", "theFile").ReadByte()
+sqlstore.Read("theFolder", "theFile").Run().ReadByte()
 
-sqlstore.GetFolders()
-sqlstore.GetFiles("theFolder")
+sqlstore.GetFolders().Run()
+sqlstore.GetFiles("theFolder").Run()
+
+
+
+
+
+#r "System.Transactions"
+
+let sqlc = new SqlConnection(conn)
+sqlc.Open()
+
+sqlc.Dispose()
+
 
 typeof<SqlServerStoreFactory>.FullName
 typeof<SqlServerStoreFactory>.Assembly.FullName
-IoC.Resolve<IStore>()
-
-let rt = MBrace.InitLocal 3
-
-let x = newRef 84 |> MBrace.RunLocal
-
-x.Value
+//IoC.Resolve<IStore>()
+//
+//let rt = MBrace.InitLocal 3
+//
+//let x = newRef 84 |> MBrace.RunLocal
+//
+//x.Value
