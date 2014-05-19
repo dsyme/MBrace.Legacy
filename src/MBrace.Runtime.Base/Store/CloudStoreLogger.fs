dee1b14514cd76001e6b24c3d60acc206004ea30
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-05-20 02:17:22 +0300

    log tests

---
+++ src/MBrace.Runtime.Base/Store/CloudStoreLogger.fs
@@ -0,0 +1,138 @@
+﻿namespace Nessos.MBrace.Runtime.Store
+
+    open System
+    open System.IO
+    open System.Threading
+
+    open Newtonsoft.Json
+
+    open Nessos.MBrace.Utils
+    open Nessos.MBrace.Utils.Logging
+    open Nessos.MBrace.Utils.Retry
+
+    open Nessos.MBrace
+    open Nessos.MBrace.Core
+    open Nessos.MBrace.Runtime
+
+    type private Message = 
+        | EnQueue of LogEntry
+        | Flush of AsyncReplyChannel<exn option>
+
+    type CloudStoreLogger (store : ICloudStore, container : string, ?minInterval : int, ?maxInterval : int, ?minEntries : int) =
+        let minInterval = defaultArg minInterval 500
+        let maxInterval = defaultArg maxInterval 5000
+        let minEntries  = defaultArg minEntries 20
+
+        do if maxInterval < minInterval then invalidArg "interval" "invalid intervals."
+
+        static let getNewLogfileName () = 
+            let id = Guid.NewGuid()
+            sprintf "%O.log" id
+
+        static let isLogFile (f : string) = f.EndsWith(".log")
+
+        let jsonSerializer = Newtonsoft.Json.JsonSerializer.Create()
+
+        let serializeLogs (entries : LogEntry []) (stream : Stream) = async { 
+            use sw = new StreamWriter(stream)
+            use jw = new JsonTextWriter(sw)
+            do jsonSerializer.Serialize(jw, entries)
+        }
+
+        let deserializeLogs (stream : Stream) = async {
+            use sr = new StreamReader(stream)
+            use jr = new JsonTextReader(sr)
+            return jsonSerializer.Deserialize<LogEntry []>(jr)
+        }
+
+
+        let flush (entries : LogEntry []) = async {
+            let file = getNewLogfileName()
+            do! store.CreateImmutable(container, file, serializeLogs entries, asFile = true)
+        }
+
+        let fetch () = async {
+            let! files = store.GetAllFiles(container)
+
+            let readEntries (f : string) = async {
+                use! stream = store.ReadImmutable(container, f)
+                return! deserializeLogs stream
+            }
+
+            let! entries =
+                files
+                |> Array.filter isLogFile
+                |> Array.map readEntries
+                |> Async.Parallel
+
+            return
+                entries
+                |> Array.concat
+                |> Array.sortBy(fun e -> e.Date)
+        }
+
+        let cts = new CancellationTokenSource()
+        let gatheredLogs = new ResizeArray<LogEntry> ()
+
+        let rec loop (mbox : MailboxProcessor<Message>) = async {
+            let! msg = mbox.Receive()
+
+            match msg with
+            | EnQueue item -> gatheredLogs.Add item
+            | Flush ch -> 
+                try
+                    if gatheredLogs.Count > 0 then
+                        do! flush <| gatheredLogs.ToArray()
+                        gatheredLogs.Clear()
+
+                    ch.Reply None
+
+                with e ->
+                    ch.Reply <| Some e
+                            
+            return! loop mbox
+        }
+
+        let batch = new MailboxProcessor<_>(loop, cts.Token)
+
+        let rec flusher interval = async {
+
+            let sleepAndRecurseWith i = async {
+                do! Async.Sleep minInterval
+                return! flusher i
+            }
+
+            if interval > maxInterval || gatheredLogs.Count > minEntries then
+                let! r = batch.PostAndAsyncReply Flush
+                match r with
+                | None -> return! sleepAndRecurseWith 0
+                | Some exn ->
+                    return! sleepAndRecurseWith (interval + minInterval)
+
+            else
+                return! sleepAndRecurseWith (interval + minInterval)
+        }
+
+        member __.Start () =
+            batch.Start()
+            do Async.Start(flusher 0, cts.Token)
+
+        member self.LogEntry (entry : LogEntry) =
+            batch.Post(EnQueue entry)
+
+        member self.Flush () =
+            match batch.PostAndReply Flush with
+            | None -> ()
+            | Some e -> raise e
+
+        member self.FetchLogs () : Async<LogEntry []> = 
+            fetch ()
+
+        member self.DeleteLogs () : Async<unit> =
+            store.DeleteContainer(container)
+
+        interface ILogger with
+            member this.LogEntry e = this.LogEntry e
+
+        interface IDisposable with
+            member __.Dispose () = cts.Cancel()
--- src/MBrace.Utils/Logger.fs
+++ src/MBrace.Utils/Logger.fs
@@ -1,65 +1,29 @@
-namespace Nessos.MBrace.Utils
+namespace Nessos.MBrace.Utils.Logging
 
     open System
     open System.IO
     open System.Threading
-    open System.Collections.Generic
-    open System.Collections.Concurrent
+
     open Microsoft.FSharp.Control
-    open Nessos.MBrace.Core
 
-    [<AbstractClass>]
-    type ILogger () =
-        abstract LogEntry : LogEntry -> unit        
-
-        member l.Log txt lvl = l.LogEntry (SystemLog (txt, lvl, DateTime.Now))
-        member l.Logf lvl fmt = Printf.ksprintf (fun str -> l.Log str lvl) fmt
-        member l.LogWithException (exn : exn) txt lvl =
-            let txt' = sprintf' "%s\nException=%s" txt <| exn.ToString ()
-            l.LogEntry <| SystemLog (txt', lvl, DateTime.Now)
-        member l.LogInfo txt = l.Log txt LogLevel.Info
-        member l.LogError exn txt = l.LogWithException exn txt LogLevel.Error
+    type ILogger =
+        abstract LogEntry : LogEntry -> unit
 
     and LogEntry = 
-        | SystemLog of string * LogLevel * DateTime
-        | UserLog of string
-        | Trace of TraceInfo
-
+        {
+            Message : string
+            Level : LogLevel
+            Date : DateTime
+        }
     with
-        member e.Print(?showDate) =
-            let showDate = defaultArg showDate false
-            match e with
-            | SystemLog(txt,lvl,date) ->
-                if showDate then
-                    sprintf' "[%s] %s : %s" <| date.ToString("yyyy-MM-dd H:mm:ss") <| lvl.ToString() <| txt
-                else sprintf' "%s : %s" <| lvl.ToString() <| txt
-            | UserLog info -> info
-//                Nessos.MBrace.Utils.String.string {
-////                    if showDate 
-////                    then yield sprintf' "[%s] " <| info.DateTime.ToString("yyyy-MM-dd H:mm:ss")
-////                    yield sprintf' "Pid %A " info.ProcessId
-////                    yield sprintf' "TaskId %A " info.TaskId
-////                    yield sprintf' "%s\n" info.Message 
-//                } |> Nessos.MBrace.Utils.String.String.build 
-            | Trace info ->
-                Nessos.MBrace.Utils.String.string {
-//                    if showDate 
-//                    then yield sprintf' "[%s] " <| info.DateTime.ToString("yyyy-MM-dd H:mm:ss")
-//                    if info.File.IsSome then yield sprintf' "File %A " info.File.Value
-//                    if info.Line.IsSome then yield sprintf' "Line %A " info.Line.Value
-                    if info.Function.IsSome then yield sprintf' "Function %A" info.Function.Value
-                    yield "\n"
-                    yield sprintf' "%s\n" info.Message
-                    if info.Environment <> null && not <| Seq.isEmpty info.Environment then
-                        yield sprintf' "--- Begin {m}brace dump ---\n"
-                        for KeyValue (var, str) in info.Environment do yield sprintf' "val %s = %s\n" var str
-                        yield sprintf' "--- End {m}brace dump ---\n" 
-                } |> Nessos.MBrace.Utils.String.String.build 
+        member e.Print(showDate : bool) =
+            if showDate then
+                let date = e.Date.ToString("yyyy-MM-dd H:mm:ss")
+                sprintf "[%s] %O : %s" date e.Level e.Message
+            else 
+                sprintf "%O : %s" e.Level e.Message
 
-    and 
-        [<CustomEquality>]
-        [<CustomComparison>]
-        LogLevel = 
+    and LogLevel = 
         | Info
         | Warning
         | Error
@@ -81,112 +45,169 @@ namespace Nessos.MBrace.Utils
             | 0 -> Info
             | 1 -> Warning
             | 2 -> Error
-            | i -> raise <| new ArgumentException(sprintf' "Invalid log level %d." i)
+            | i -> invalidArg "i" "invalid log level"
 
-        member internal x.CompareTo (y : obj) =
-            match y with
-            | :? LogLevel as y -> x.Value - y.Value
-            | _ -> raise <| new ArgumentException("Invalid argument","y")
+//        member internal x.CompareTo (y : obj) =
+//            match y with
+//            | :? LogLevel as y -> x.Value - y.Value
+//            | _ -> raise <| new ArgumentException("Invalid argument","y")
+//
+//        override x.Equals y = x.CompareTo y = 0
+//        override x.GetHashCode () = x.Value
+//        interface IComparable with member x.CompareTo y = x.CompareTo y
 
-        override x.Equals y = x.CompareTo y = 0
-        override x.GetHashCode () = x.Value
-        interface IComparable with member x.CompareTo y = x.CompareTo y
-            
+//    [<AbstractClass>]
+//    type ILogger () =
+//        abstract LogEntry : LogEntry -> unit        
+//
+//        member l.Log txt lvl = l.LogEntry (SystemLog (txt, lvl, DateTime.Now))
+//        member l.Logf lvl fmt = Printf.ksprintf (fun str -> l.Log str lvl) fmt
+//        member l.LogWithException (exn : exn) txt lvl =
+//            let txt' = sprintf' "%s\nException=%s" txt <| exn.ToString ()
+//            l.LogEntry <| SystemLog (txt', lvl, DateTime.Now)
+//        member l.LogInfo txt = l.Log txt LogLevel.Info
+//        member l.LogError exn txt = l.LogWithException exn txt LogLevel.Error
 
-    module Logger =
+//    and LogEntry = 
+//        | SystemLog of string * LogLevel * DateTime
+//        | UserLog of string
+//        | Trace of TraceInfo
+//
+//    with
+//        member e.Print(?showDate) =
+//            let showDate = defaultArg showDate false
+//            match e with
+//            | SystemLog(txt,lvl,date) ->
+//                if showDate then
+//                    sprintf' "[%s] %s : %s" <| date.ToString("yyyy-MM-dd H:mm:ss") <| lvl.ToString() <| txt
+//                else sprintf' "%s : %s" <| lvl.ToString() <| txt
+//            | UserLog info -> info
+////                Nessos.MBrace.Utils.String.string {
+//////                    if showDate 
+//////                    then yield sprintf' "[%s] " <| info.DateTime.ToString("yyyy-MM-dd H:mm:ss")
+//////                    yield sprintf' "Pid %A " info.ProcessId
+//////                    yield sprintf' "TaskId %A " info.TaskId
+//////                    yield sprintf' "%s\n" info.Message 
+////                } |> Nessos.MBrace.Utils.String.String.build 
+//            | Trace info ->
+//                Nessos.MBrace.Utils.String.string {
+////                    if showDate 
+////                    then yield sprintf' "[%s] " <| info.DateTime.ToString("yyyy-MM-dd H:mm:ss")
+////                    if info.File.IsSome then yield sprintf' "File %A " info.File.Value
+////                    if info.Line.IsSome then yield sprintf' "Line %A " info.Line.Value
+//                    if info.Function.IsSome then yield sprintf' "Function %A" info.Function.Value
+//                    yield "\n"
+//                    yield sprintf' "%s\n" info.Message
+//                    if info.Environment <> null && not <| Seq.isEmpty info.Environment then
+//                        yield sprintf' "--- Begin {m}brace dump ---\n"
+//                        for KeyValue (var, str) in info.Environment do yield sprintf' "val %s = %s\n" var str
+//                        yield sprintf' "--- End {m}brace dump ---\n" 
+//                } |> Nessos.MBrace.Utils.String.String.build  
 
-        let log txt lvl (l : ILogger) = l.Log txt lvl
-        let logWithException (exn : exn) txt lvl (l : ILogger) = l.LogWithException exn txt lvl
-        let logInfo txt (l : ILogger) = l.LogInfo txt
-        let logError (exn : exn) txt (l : ILogger) = l.LogError exn txt
+    // Logger primitives
 
-        let logEntry entry (l : ILogger) = l.LogEntry entry
-        let safelog entry (l : ILogger) = try l.LogEntry entry with _ -> ()
+    type NullLogger () =
+        interface ILogger with
+            member __.LogEntry _ = ()
 
-        // Logger primitives
+    type ConsoleLogger (?showDate) =
+        let showDate = defaultArg showDate false
 
-        type NullLogger () =
-            inherit ILogger ()
-            override m.LogEntry _ = ()
-
-        type ConsoleLogger (?showDate) =
-            inherit ILogger ()
-            let showDate = defaultArg showDate false
-
-            override m.LogEntry e =
+        interface ILogger with
+            member __.LogEntry e =
                 Console.WriteLine (e.Print(showDate))
 
-        type FileLogger (path : string, ?initMsg : string, ?showDate, ?append) =
-            inherit ILogger ()
+    type FileLogger (path : string, ?initMsg : string, ?showDate, ?append) =
 
-            let showDate = defaultArg showDate true
-            let writer = TextWriter.Synchronized(new StreamWriter(path, defaultArg append true))
+        let showDate = defaultArg showDate true
+        let writer = TextWriter.Synchronized(new StreamWriter(path, defaultArg append true))
 
-            do initMsg |> Option.iter writer.WriteLine
+        do initMsg |> Option.iter writer.WriteLine
 
-            override __.LogEntry e =
+        interface ILogger with
+            member __.LogEntry e =
                 writer.WriteLine (e.Print(showDate))
                 writer.Flush ()
              
-            interface IDisposable with
-                member __.Dispose () = writer.Close ()
-
-        type InMemoryLogger () =
-            inherit ILogger ()
-            let mutable container = new ConcurrentQueue<LogEntry> ()
-
-            override __.LogEntry e = container.Enqueue e
-
-            member __.Dump () = container.ToArray()
-            member __.Clear () = container <- new ConcurrentQueue<LogEntry> ()
+        interface IDisposable with
+            member __.Dispose () = writer.Close ()
         
-        type AsyncLogger (underlying : ILogger) =
-            inherit ILogger ()
+    type AsyncLogger (underlying : ILogger) =
 
-            let cts = new CancellationTokenSource ()
+        let cts = new CancellationTokenSource ()
 
-            let rec behaviour (inbox : MailboxProcessor<LogEntry>) =
-                async {
-                    let! entry = inbox.Receive ()
+        let rec behaviour (inbox : MailboxProcessor<LogEntry>) =
+            async {
+                let! entry = inbox.Receive ()
 
-                    safelog entry underlying
+                try underlying.LogEntry entry
+                with _ -> ()
                 
-                    return! behaviour inbox
-                }
+                return! behaviour inbox
+            }
             
-            let actor = MailboxProcessor.Start (behaviour, cts.Token)
+        let actor = MailboxProcessor.Start (behaviour, cts.Token)
 
-            override __.LogEntry e = actor.Post e
+        interface ILogger with
+            member __.LogEntry e = actor.Post e
 
-            interface IDisposable with
-                member __.Dispose () = cts.Cancel ()
+        interface IDisposable with
+            member __.Dispose () = cts.Cancel ()       
 
+    
+    [<RequireQualifiedAccess>]
+    module Logger =
+
+        let inline private mkEntry level message =
+            { Date = DateTime.Now ; Message = message ; Level = level }
+
+        let inline logEntry entry (l : ILogger) = l.LogEntry entry
+        let inline log txt lvl (l : ILogger) = l.LogEntry <| mkEntry lvl txt
+        let inline logWithException (exn : exn) txt lvl (l : ILogger) = 
+            let message = sprintf "%s:\nException=%O" txt  exn
+            l.LogEntry <| mkEntry lvl message
+
+        let inline logInfo txt (l : ILogger) = l.LogEntry <| mkEntry Info txt
+        let inline logError txt (l : ILogger) = l.LogEntry <| mkEntry Error txt
+        let inline logWarning txt (l : ILogger) = l.LogEntry <| mkEntry Warning txt
+        
+        let inline logSafe e (l : ILogger) = try l.LogEntry e with _ -> ()
 
         let createNullLogger () = (new NullLogger ()) :> ILogger
         let createConsoleLogger () = (new ConsoleLogger ()) :> ILogger
         let createFileLogger path = (new FileLogger (path)) :> ILogger
-        let createInMemoryLogger () = (new InMemoryLogger ()) :> ILogger
 
         // combinators
 
         /// <summary>Constructs an abstract Logger</summary>
         /// <param name="append">the append log entry behavior.</param>
-        let create (append : LogEntry -> unit) =
+        let inline create (append : LogEntry -> unit) =
             {
-                new ILogger () with
+                new ILogger with
                     member m.LogEntry e = append e
             }
 
         let wrapAsync (l : ILogger) = (new AsyncLogger(l)) :> ILogger
 
         let propagate (loggers : ILogger list) =
-            create (fun e -> List.iter (safelog e) loggers)
+            create (fun e -> for l in loggers do logSafe e l)
 
-        let convert convertF (logger : ILogger) =
+        let map convertF (logger : ILogger) =
             create (fun e -> logEntry (convertF e) logger)
 
         let filter (filterP : LogEntry -> bool) (logger : ILogger) =
             create (fun e -> if filterP e then logEntry e logger)
 
         let maxLogLevel (lvl : LogLevel) (logger : ILogger) = 
-            filter (fun (SystemLog(_,lvl',_)) -> lvl <= lvl') logger
+            filter (fun e -> e.Level <= lvl) logger
+
+
+    [<AutoOpen>]
+    module LogUtils =
+
+        type ILogger with
+            member l.Log (text : string) (lvl : LogLevel) = Logger.log text lvl l
+            member l.LogWithException exn text lvl = Logger.logWithException exn text lvl
+            member l.LogInfo text = Logger.logInfo text l
+            member l.LogError text = Logger.logError text l
+            member l.LogWarning text = Logger.logWarning text l
