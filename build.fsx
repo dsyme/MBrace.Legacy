14e2261e3261dce9689b31ca28314f34a8b03f70
krontogiannis <rodoconst@hotmail.com>
2014-05-20 23:32:45 +0300

    Fixed nunit target, added nunit-runners package

---
+++ build.fsx
@@ -0,0 +1,101 @@
+﻿// include Fake lib
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+open System
+open System.IO
+
+
+let project = "MBrace"
+let testAssemblies = ["bin/MBrace.Runtime.Tests.dll"]
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let excludedStoresCategory = "CustomStores"
+
+// --------------------------------------------------------------------------------------
+// Clean and restore packages
+
+
+Target "RestorePackages" (fun _ ->
+    !! "./**/packages.config"
+    |> Seq.iter (RestorePackage (fun p -> { p with ToolPath = "./.nuget/NuGet.exe" }))
+)
+
+Target "Clean" (fun _ ->
+    CleanDirs (!! "**/bin/Release/")
+    CleanDir "bin/"
+)
+
+// --------------------------------------------------------------------------------------
+// Build
+
+
+let configuration = environVarOrDefault "Configuration" "Release"
+
+Target "Build" (fun _ ->
+    // Build the rest of the project
+    { BaseDirectory = __SOURCE_DIRECTORY__
+      Includes = [ project + ".sln" ]
+      Excludes = [] } 
+    |> MSBuild "" "Build" ["Configuration", configuration]
+    |> Log "AppBuild-Output: "
+)
+
+// --------------------------------------------------------------------------------------
+// Run the unit tests using test runner & kill test runner when complete
+
+
+Target "RunTestsExcludingCustomStores" (fun _ ->
+    let nunitVersion = GetPackageVersion "packages" "NUnit.Runners"
+    let nunitPath = sprintf "packages/NUnit.Runners.%s/tools" nunitVersion
+    ActivateFinalTarget "CloseTestRunner"
+        
+    testAssemblies
+    |> NUnit (fun p -> 
+        { p with
+            Framework = "v4.0.30319"
+            ToolPath = nunitPath
+            DisableShadowCopy = true
+            TimeOut = TimeSpan.FromMinutes 20.
+            ExcludeCategory = excludedStoresCategory
+            OutputFile = "TestResults.xml" })
+)
+
+Target "RunTests" (fun _ ->
+    let nunitVersion = GetPackageVersion "packages" "NUnit.Runners"
+    let nunitPath = sprintf "packages/NUnit.Runners.%s/tools" nunitVersion
+    ActivateFinalTarget "CloseTestRunner"
+        
+    testAssemblies
+    |> NUnit (fun p -> 
+        { p with
+            Framework = "v4.0.30319"
+            ToolPath = nunitPath
+            DisableShadowCopy = true
+            TimeOut = TimeSpan.FromMinutes 20.
+            OutputFile = "TestResults.xml" })
+)
+
+FinalTarget "CloseTestRunner" (fun _ ->  
+    ProcessHelper.killProcess "nunit-agent.exe"
+)
+
+// --------------------------------------------------------------------------------------
+// Run all targets by default. Invoke 'build <Target>' to override
+Target "Default" DoNothing
+Target "DefaultWithStores" DoNothing
+
+"Clean"
+  ==> "RestorePackages"
+  ==> "Build"
+  ==> "RunTestsExcludingCustomStores"
+  ==> "Default"
+
+"Clean"
+  ==> "RestorePackages"
+  ==> "Build"
+  ==> "RunTests"
+  ==> "DefaultWithStores"
+
+// start build
+RunTargetOrDefault "Default"
