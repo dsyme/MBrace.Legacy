c76a1ed419cad424b4f03fdd8089f8f2b88928aa
krontogiannis <rodoconst@hotmail.com>
2014-05-29 18:15:44 +0300

    StoreClient

--- src/MBrace.Client/StoreClient.fs
+++ src/MBrace.Client/StoreClient.fs
@@ -22,9 +22,52 @@
 
 
     type StoreClient internal (config : PrimitiveConfiguration) =
-        class end
-//        member this.CreateCloudRefAsync(container : Container,  value : 'T) =
-//            config.CloudRefProvider.Create
 
+        //---------------------------------------------------------------------------------
+        // CloudRef
 
+        member this.CreateCloudRefAsync(container : Container,  value : 'T) =
+            config.CloudRefProvider.Create(container, Guid.NewGuid().ToString(), value)
+
+        member this.GetCloudRefsAsync(container : Container) =
+            config.CloudRefProvider.GetContainedRefs(container)
+
+        member this.GetCloudRefAsync(container : Container, id : Id) =
+            config.CloudRefProvider.GetExisting(container, id)
+            
+
+        //---------------------------------------------------------------------------------
+        // CloudSeq
+        member this.CreateCloudSeqAsync(container : Container,  values : 'T seq) =
+            config.CloudSeqProvider.Create(container, Guid.NewGuid().ToString(), values)
+
+        member this.GetCloudSeqsAsync(container : Container) =
+            config.CloudSeqProvider.GetContainedSeqs(container)
+
+        member this.GetCloudSeqAsync(container : Container, id : Id) =
+            config.CloudSeqProvider.GetExisting(container, id)
+
+        //---------------------------------------------------------------------------------
+        // CloudFile
+        member this.CreateCloudFileAsync(container : Container, writer : Stream -> Async<unit>) =
+            config.CloudFileProvider.Create(container, Guid.NewGuid().ToString(), writer)
+
+        member this.GetCloudFilesAsync(container : Container) =
+            config.CloudFileProvider.GetContainedFiles(container)
+
+        member this.GetCloudFileAsync(container : Container, id : Id) =
+            config.CloudFileProvider.GetExisting(container, id)
+
+        //---------------------------------------------------------------------------------
+        // MutableCloudRef
+
+        member this.CreateMutableCloudRefAsync(container : Container,  value : 'T) =
+            config.MutableCloudRefProvider.Create(container, Guid.NewGuid().ToString(), value)
+
+        member this.GetMutableCloudRefsAsync(container : Container) =
+            config.MutableCloudRefProvider.GetContainedRefs(container)
+
+        member this.GetMutableCloudRefAsync(container : Container, id : Id) =
+            config.MutableCloudRefProvider.GetExisting(container, id)
+            
 
---
+++ src/MBrace.Client/kostas.fsx
@@ -0,0 +1,11 @@
+﻿
+#load "preamble.fsx"
+open Nessos.MBrace
+open Nessos.MBrace.Client
+
+let rt = MBrace.InitLocal 3
+
+let f = cloud.Return 42
+
+rt.Run f
+
