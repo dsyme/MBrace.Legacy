17cab5a5ca178b12f6cd83d04b322fb91b127fcf
krontogiannis <rodoconst@hotmail.com>
2014-05-29 17:52:16 +0300

    erik fsx

---
+++ src/MBrace.Client/StoreClient.fs
@@ -0,0 +1,30 @@
+﻿namespace Nessos.MBrace.Client
+
+    open System
+    open System.Reflection
+    open System.IO
+
+    open Nessos.Thespian.ConcurrencyTools
+
+    open Nessos.FsPickler
+    open Nessos.UnionArgParser
+    open Nessos.Vagrant
+    open Nessos.Thespian.Serialization
+    open Nessos.Thespian.Remote
+
+    open Nessos.MBrace
+    open Nessos.MBrace.Core
+    open Nessos.MBrace.Utils
+    open Nessos.MBrace.Utils.Retry
+    open Nessos.MBrace.Runtime
+    open Nessos.MBrace.Runtime.Logging
+    open Nessos.MBrace.Runtime.Store
+
+
+    type StoreClient internal (config : PrimitiveConfiguration) =
+        class end
+//        member this.CreateCloudRefAsync(container : Container,  value : 'T) =
+//            config.CloudRefProvider.Create
+
+
+
---
+++ src/MBrace.Client/eirik.fsx
@@ -0,0 +1,6 @@
+﻿
+#load "preamble.fsx"
+open Nessos.MBrace
+open Nessos.MBrace.Client
+
+let runtime = MBrace.InitLocal 3
