556a83ff81d63ca456990259445e5cd5a735c418
krontogiannis <rodoconst@hotmail.com>
2014-05-30 13:54:00 +0300

    More StoreClient methods.

--- src/MBrace.Client/StoreClient.fs
+++ src/MBrace.Client/StoreClient.fs
@@ -1,45 +1,46 @@
 ﻿namespace Nessos.MBrace.Client
 
     open System
-    open System.Reflection
     open System.IO
-
-    open Nessos.Thespian.ConcurrencyTools
-
-    open Nessos.FsPickler
-    open Nessos.UnionArgParser
-    open Nessos.Vagrant
-    open Nessos.Thespian.Serialization
-    open Nessos.Thespian.Remote
 
     open Nessos.MBrace
     open Nessos.MBrace.Core
-    open Nessos.MBrace.Utils
-    open Nessos.MBrace.Utils.Retry
     open Nessos.MBrace.Runtime
-    open Nessos.MBrace.Runtime.Logging
     open Nessos.MBrace.Runtime.Store
 
-
+    /// Provides methods for interacting with the store and the primitives without the need for a runtime.
     type StoreClient internal (config : PrimitiveConfiguration) =
+
+        let newId () = Guid.NewGuid().ToString()
 
         //---------------------------------------------------------------------------------
         // CloudRef
 
-        member this.CreateCloudRefAsync(container : Container,  value : 'T) =
-            config.CloudRefProvider.Create(container, Guid.NewGuid().ToString(), value)
-
+        member this.CreateCloudRefAsync(container : Container, value : 'T) =
+            config.CloudRefProvider.Create(container, newId(), value)
+            
         member this.GetCloudRefsAsync(container : Container) =
             config.CloudRefProvider.GetContainedRefs(container)
 
         member this.GetCloudRefAsync(container : Container, id : Id) =
             config.CloudRefProvider.GetExisting(container, id)
+
+
+        member this.CreateCloudRef(container : Container,  value : 'T) =
+            Async.RunSynchronously <| config.CloudRefProvider.Create(container, newId(), value)
+
+        member this.GetCloudRefs(container : Container) =
+            Async.RunSynchronously <| config.CloudRefProvider.GetContainedRefs(container)
+
+        member this.GetCloudRef(container : Container, id : Id) =
+            Async.RunSynchronously <|config.CloudRefProvider.GetExisting(container, id)
             
 
         //---------------------------------------------------------------------------------
         // CloudSeq
+
         member this.CreateCloudSeqAsync(container : Container,  values : 'T seq) =
-            config.CloudSeqProvider.Create(container, Guid.NewGuid().ToString(), values)
+            config.CloudSeqProvider.Create(container, newId(), values)
 
         member this.GetCloudSeqsAsync(container : Container) =
             config.CloudSeqProvider.GetContainedSeqs(container)
@@ -47,22 +48,43 @@
         member this.GetCloudSeqAsync(container : Container, id : Id) =
             config.CloudSeqProvider.GetExisting(container, id)
 
+
+        member this.CreateCloudSeq(container : Container,  values : 'T seq) =
+            Async.RunSynchronously <| config.CloudSeqProvider.Create(container, newId(), values)
+
+        member this.GetCloudSeqs(container : Container) =
+            Async.RunSynchronously <| config.CloudSeqProvider.GetContainedSeqs(container)
+
+        member this.GetCloudSeq(container : Container, id : Id) =
+            Async.RunSynchronously <| config.CloudSeqProvider.GetExisting(container, id)
+
         //---------------------------------------------------------------------------------
         // CloudFile
+
         member this.CreateCloudFileAsync(container : Container, writer : Stream -> Async<unit>) =
-            config.CloudFileProvider.Create(container, Guid.NewGuid().ToString(), writer)
+            config.CloudFileProvider.Create(container, newId(), writer)
 
         member this.GetCloudFilesAsync(container : Container) =
             config.CloudFileProvider.GetContainedFiles(container)
 
         member this.GetCloudFileAsync(container : Container, id : Id) =
             config.CloudFileProvider.GetExisting(container, id)
+            
+
+        member this.CreateCloudFile(container : Container, writer : Stream -> Async<unit>) =
+            Async.RunSynchronously <| config.CloudFileProvider.Create(container, newId(), writer)
+
+        member this.GetCloudFiles(container : Container) =
+            Async.RunSynchronously <| config.CloudFileProvider.GetContainedFiles(container)
+
+        member this.GetCloudFile(container : Container, id : Id) =
+            Async.RunSynchronously <| config.CloudFileProvider.GetExisting(container, id)
 
         //---------------------------------------------------------------------------------
         // MutableCloudRef
 
         member this.CreateMutableCloudRefAsync(container : Container,  value : 'T) =
-            config.MutableCloudRefProvider.Create(container, Guid.NewGuid().ToString(), value)
+            config.MutableCloudRefProvider.Create(container, newId(), value)
 
         member this.GetMutableCloudRefsAsync(container : Container) =
             config.MutableCloudRefProvider.GetContainedRefs(container)
@@ -71,3 +93,12 @@
             config.MutableCloudRefProvider.GetExisting(container, id)
             
 
+        member this.CreateMutableCloudRef(container : Container,  value : 'T) =
+            Async.RunSynchronously <| config.MutableCloudRefProvider.Create(container, newId(), value)
+
+        member this.GetMutableCloudRefs(container : Container) =
+            Async.RunSynchronously <| config.MutableCloudRefProvider.GetContainedRefs(container)
+
+        member this.GetMutableCloudRef(container : Container, id : Id) =
+            Async.RunSynchronously <| config.MutableCloudRefProvider.GetExisting(container, id)
+            
