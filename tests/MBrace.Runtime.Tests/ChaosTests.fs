c17847e0357d703f3021f6568b96b9dbc16ed51e
krontogiannis <rodoconst@hotmail.com>
2014-06-11 16:07:55 +0300

    Chaos monkey tests

--- tests/MBrace.Runtime.Tests/ChaosTests.fs
+++ tests/MBrace.Runtime.Tests/ChaosTests.fs
@@ -1,4 +1,6 @@
-﻿namespace Nessos.MBrace.Runtime.Tests
+﻿#nowarn "0044" // Cloud blocks should be wrapped in quotation literals for better debug support.
+
+namespace Nessos.MBrace.Runtime.Tests
 
     open Nessos.MBrace
     open Nessos.MBrace.Client
@@ -11,6 +13,7 @@
     open System.IO
     open Microsoft.FSharp.Quotations
     open FsUnit
+    open FsCheck
 
     [<TestFixture; Category("ChaosMonkeyTests")>]
     module ``Chaos Monkey Tests`` =
@@ -21,30 +24,37 @@
             | Attach 
             | Detatch
             | KillViolently of SpawnOnKill
-            | Nothing
 
         type ChaosMonkeyConfiguration = {
-            MinKillWaitTime : int
-            MaxKillWaitTime : int
+            MinSleepTime    : int
+            MaxSleepTime    : int
             MinKillCount    : int
             MaxKillCount    : int
-            Actions         : ChaosAction list
+            MinNodesCount   : int
+            MaxNodesCount   : int
         }
 
-        let rec chaosMonkey (conf: ChaosMonkeyConfiguration) (runtime: MBraceRuntime): Async<unit> = async {
-            match conf.Actions with
+        let rec chaosMonkey (conf: ChaosMonkeyConfiguration) 
+                            (actions : ChaosAction list) 
+                            (runtime: MBraceRuntime): Async<unit> = async {
+            match actions with
             | [] -> 
-                return ()
+                printfn "Chaos Monkey done"
             | action :: rest ->
                 let random = new Random(System.DateTime.Now.Millisecond)
-                let waitTime = random.Next(conf.MinKillWaitTime, conf.MaxKillWaitTime + 1)
-                printfn "Waiting for %d seconds for next action..." waitTime
-                do! Async.Sleep (waitTime*1000)
-            
+                let nodeCount = runtime.Nodes.Length
+                printfn "NodeCount %d" nodeCount
+                printfn "Current action %A" action
+
                 match action with
                 | KillViolently spawnOnKill ->
                     let processes = Process.GetProcessesByName("mbraced")
                     let killCount = random.Next(conf.MinKillCount, conf.MaxKillCount + 1)
+                    let killCount =
+                        if nodeCount - killCount < conf.MinNodesCount 
+                        then nodeCount - conf.MinNodesCount
+                        else killCount
+
                     printfn "Killing %d processes" killCount
                     let k = ref killCount
                     while !k <> 0 do
@@ -63,6 +73,12 @@
                         | FlipACoin(Rand(min, max)) -> if random.Next(0, 2) = 0 then random.Next(min, max) else 0
 
                     if spawnCount > 0 then
+                        let spawnCount = 
+                            let nodeCount' = runtime.Nodes.Length
+                            if nodeCount' + spawnCount > conf.MaxNodesCount 
+                            then conf.MaxNodesCount - nodeCount'
+                            else spawnCount
+
                         printfn "Spawning %d nodes..." spawnCount
                         let nodes = Node.SpawnMultiple spawnCount
                         printfn "Attaching nodes..."
@@ -70,46 +86,76 @@
                             do! runtime.AttachAsync nodes
                         with e -> printfn "%A" e
                 | Attach ->
-                    let node = Node.SpawnMultiple 1
-                    try
-                        do! runtime.AttachAsync node
-                    with e -> printfn "%A" e
+                    if nodeCount + 1 > conf.MaxNodesCount then 
+                        printfn "Ignoring Attach"
+                    else
+                        let node = Node.SpawnMultiple 1
+                        try
+                            do! runtime.AttachAsync node
+                        with e -> printfn "%A" e
                 | Detatch ->
-                    let node = runtime.Nodes.[random.Next(0, runtime.Nodes.Length-1)]
-                    try
-                        do! runtime.DetachAsync node
-                    with e -> printfn "%A" e
-                | Nothing ->
-                    ()
-                return! chaosMonkey {conf with Actions = rest } runtime
+                    if nodeCount - 1 < conf.MinNodesCount then
+                        printfn "Ignoring Detatch"
+                    else
+                        let node = runtime.Nodes.[random.Next(0, runtime.Nodes.Length-1)]
+                        printfn "Detaching %A" node
+                        try
+                            do! runtime.DetachAsync node
+                        with e -> printfn "%A" e
+
+                let waitTime = random.Next(conf.MinSleepTime, conf.MaxKillCount + 1)                
+                printfn "Waiting for %d seconds for next action..." waitTime
+                do! Async.Sleep (waitTime*1000)
+
+                return! chaosMonkey conf rest runtime
         }
 
-        [<Cloud>]
-        let rec bin (depth : int)  = cloud {
-            if depth = 0 then 
-                return 1
-            else 
-                let! (l,r) = bin (depth-1) <||> bin (depth-1) 
-                return l + r
-        }
+        let defaultConf = 
+            {
+                MinSleepTime    = 2
+                MaxSleepTime    = 10
+                MinKillCount    = 1
+                MaxKillCount    = 10
+                MinNodesCount   = 3
+                MaxNodesCount   = 10
+            }
+            
+        let checkF (cexpr : Cloud<'T>) (result : 'T) =
+            MBraceSettings.MBracedExecutablePath <- Path.Combine(Directory.GetCurrentDirectory(), "mbraced.exe")
+            printfn "Using conf %A" defaultConf
+            let quick = { FsCheck.Config.Quick with MaxTest = 10 }
+            FsCheck.Check.One(quick, (fun (actions : ChaosAction list) ->
+                printfn "%s" <| String.init 120 (fun _ -> "_")
+                if List.isEmpty actions then
+                    printfn "Ignoring empty action stack"
+                else
+                    printfn "Booting runtime"
+                    let rt = MBrace.InitLocal(4, debug = true)
+                    let chaos = chaosMonkey defaultConf actions rt
+                    let cts = new CancellationTokenSource()
+                    try 
+                        printfn "Creating process"
+                        let ps = rt.CreateProcess(cexpr)
+                        printfn "Using action stack %A" actions
+                        printfn "STARTING CHAOS MONKEY"
+                        Async.Start(chaos, cts.Token)
+                        ps.AwaitResult() |> should equal result
+                        printfn "Process completed"
+                    finally
+                        cts.Cancel()
+                        rt.Kill()
+                        Process.GetProcessesByName("mbraced") 
+                        |> Seq.iter (fun ps -> ps.Kill()) ))
 
         [<Test>]
         let ``Binary rec``() =
-            let conf = {
-                MinKillWaitTime = 10
-                MaxKillWaitTime = 35
-                MinKillCount = 1
-                MaxKillCount = 1
-                Actions = [Nothing] // Create with fscheck
-                }
-
-            do MBraceSettings.MBracedExecutablePath <- Path.Combine(Directory.GetCurrentDirectory(), "mbraced.exe")
-            let rt = MBrace.InitLocal(3, debug = true)
-
-
-            let chaos = chaosMonkey conf rt
-            let cts = new CancellationTokenSource()
-            Async.Start(chaos, cts.Token)
-
-            rt.Run <@ bin 10 @> |> should equal 1024
-            cts.Cancel()
+            let rec bin (depth : int)  = cloud {
+                if depth = 0 then 
+                    return 1
+                else 
+                    let! (l,r) = bin (depth-1) <||> bin (depth-1) 
+                    return l + r
+            }
+         
+            let i = 9
+            checkF (bin i) (pown 2 i)
