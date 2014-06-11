43eb2f6f0dd02b179ff638b99b7b0df314dcdecb
krontogiannis <rodoconst@hotmail.com>
2014-06-11 17:39:48 +0300

    Changed quickcheck configuration to QuickThrowOnFailure

--- tests/MBrace.Runtime.Tests/ChaosTests.fs
+++ tests/MBrace.Runtime.Tests/ChaosTests.fs
@@ -46,62 +46,58 @@ namespace Nessos.MBrace.Runtime.Tests
                 printfn "NodeCount %d" nodeCount
                 printfn "Current action %A" action
 
-                match action with
-                | KillViolently spawnOnKill ->
-                    let processes = Process.GetProcessesByName("mbraced")
-                    let killCount = random.Next(conf.MinKillCount, conf.MaxKillCount + 1)
-                    let killCount =
-                        if nodeCount - killCount < conf.MinNodesCount 
-                        then nodeCount - conf.MinNodesCount
-                        else killCount
+                try
+                    match action with
+                    | KillViolently spawnOnKill ->
+                        let processes = Process.GetProcessesByName("mbraced")
+                        let killCount = random.Next(conf.MinKillCount, conf.MaxKillCount + 1)
+                        let killCount =
+                            if nodeCount - killCount < conf.MinNodesCount 
+                            then nodeCount - conf.MinNodesCount
+                            else killCount
 
-                    printfn "Killing %d processes" killCount
-                    let k = ref killCount
-                    while !k <> 0 do
-                        let i = random.Next(0, processes.Length)
-                        if not <| processes.[i].HasExited then
-                            processes.[i].Kill()
-                            printfn "Killed %A" processes.[i].Id
-                            k := !k - 1
+                        printfn "Killing %d processes" killCount
+                        let k = ref killCount
+                        while !k <> 0 do
+                            let i = random.Next(0, processes.Length)
+                            if not <| processes.[i].HasExited then
+                                processes.[i].Kill()
+                                printfn "Killed %A" processes.[i].Id
+                                k := !k - 1
             
-                    let spawnCount =
-                        match spawnOnKill with
-                        | Never -> 0
-                        | Always Same -> killCount
-                        | Always(Rand(min, max)) -> random.Next(min, max)
-                        | FlipACoin Same -> if random.Next(0, 2) = 0 then killCount else 0
-                        | FlipACoin(Rand(min, max)) -> if random.Next(0, 2) = 0 then random.Next(min, max) else 0
+                        let spawnCount =
+                            match spawnOnKill with
+                            | Never -> 0
+                            | Always Same -> killCount
+                            | Always(Rand(min, max)) -> random.Next(min, max)
+                            | FlipACoin Same -> if random.Next(0, 2) = 0 then killCount else 0
+                            | FlipACoin(Rand(min, max)) -> if random.Next(0, 2) = 0 then random.Next(min, max) else 0
 
-                    if spawnCount > 0 then
-                        let spawnCount = 
-                            let nodeCount' = runtime.Nodes.Length
-                            if nodeCount' + spawnCount > conf.MaxNodesCount 
-                            then conf.MaxNodesCount - nodeCount'
-                            else spawnCount
+                        if spawnCount > 0 then
+                            let spawnCount = 
+                                let nodeCount' = runtime.Nodes.Length
+                                if nodeCount' + spawnCount > conf.MaxNodesCount 
+                                then conf.MaxNodesCount - nodeCount'
+                                else spawnCount
 
-                        printfn "Spawning %d nodes..." spawnCount
-                        let nodes = Node.SpawnMultiple spawnCount
-                        printfn "Attaching nodes..."
-                        try
+                            printfn "Spawning %d nodes..." spawnCount
+                            let nodes = Node.SpawnMultiple spawnCount
+                            printfn "Attaching nodes..."
                             do! runtime.AttachAsync nodes
-                        with e -> printfn "%A" e
-                | Attach ->
-                    if nodeCount + 1 > conf.MaxNodesCount then 
-                        printfn "Ignoring Attach"
-                    else
-                        let node = Node.SpawnMultiple 1
-                        try
+                    | Attach ->
+                        if nodeCount + 1 > conf.MaxNodesCount then 
+                            printfn "Ignoring Attach"
+                        else
+                            let node = Node.SpawnMultiple 1
                             do! runtime.AttachAsync node
-                        with e -> printfn "%A" e
-                | Detatch ->
-                    if nodeCount - 1 < conf.MinNodesCount then
-                        printfn "Ignoring Detatch"
-                    else
-                        let node = runtime.Nodes.[random.Next(0, runtime.Nodes.Length-1)]
-                        printfn "Detaching %A" node
-                        try
+                    | Detatch ->
+                        if nodeCount - 1 < conf.MinNodesCount then
+                            printfn "Ignoring Detatch"
+                        else
+                            let node = runtime.Nodes.[random.Next(0, runtime.Nodes.Length-1)]
+                            printfn "Detaching %A" node
                             do! runtime.DetachAsync node
-                        with e -> printfn "%A" e
+                with e -> printfn "failed with %A" e
 
                 let waitTime = random.Next(conf.MinSleepTime, conf.MaxKillCount + 1)                
                 printfn "Waiting for %d seconds for next action..." waitTime
@@ -113,7 +109,7 @@ namespace Nessos.MBrace.Runtime.Tests
         let defaultConf = 
             {
                 MinSleepTime    = 2
-                MaxSleepTime    = 10
+                MaxSleepTime    = 6
                 MinKillCount    = 1
                 MaxKillCount    = 10
                 MinNodesCount   = 3
@@ -123,17 +119,17 @@ namespace Nessos.MBrace.Runtime.Tests
         let checkF (cexpr : Cloud<'T>) (result : 'T) =
             MBraceSettings.MBracedExecutablePath <- Path.Combine(Directory.GetCurrentDirectory(), "mbraced.exe")
             printfn "Using conf %A" defaultConf
-            let quick = { FsCheck.Config.Quick with MaxTest = 10 }
+            let quick = { FsCheck.Config.QuickThrowOnFailure with MaxTest = 10 }
+            
             FsCheck.Check.One(quick, (fun (actions : ChaosAction list) ->
-                printfn "%s" <| String.init 120 (fun _ -> "_")
-                if List.isEmpty actions then
-                    printfn "Ignoring empty action stack"
-                else
-                    printfn "Booting runtime"
-                    let rt = MBrace.InitLocal(4, debug = true)
-                    let chaos = chaosMonkey defaultConf actions rt
+                if not <| List.isEmpty actions then
                     use cts = new CancellationTokenSource()
                     try 
+                        printfn "\n%s" <| String.init 120 (fun _ -> "_")
+                        printf "Booting runtime..."
+                        let rt = MBrace.InitLocal(5, debug = true)
+                        printfn "done"
+                        let chaos = chaosMonkey defaultConf actions rt
                         printfn "Creating process"
                         let ps = rt.CreateProcess(cexpr)
                         printfn "Using action stack %A" actions
@@ -143,8 +139,6 @@ namespace Nessos.MBrace.Runtime.Tests
                         printfn "Process completed"
                     finally
                         cts.Cancel()
-                        rt.Ping() |> should greaterThan 0
-                        rt.Kill()
                         Process.GetProcessesByName("mbraced") 
                         |> Seq.iter (fun ps -> ps.Kill()) ))
 
@@ -158,5 +152,5 @@ namespace Nessos.MBrace.Runtime.Tests
                     return l + r
             }
          
-            let i = 9
+            let i = 10
             checkF (bin i) (pown 2 i)
