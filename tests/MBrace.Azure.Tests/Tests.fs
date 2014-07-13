9a4bf9a3cdc6bd9e68d3ebced174a41ce174db2f
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-07-13 05:38:41 +0300

    remove azure dependency from store tests

---
+++ tests/MBrace.Azure.Tests/Tests.fs
@@ -0,0 +1,16 @@
+﻿namespace Nessos.MBrace.Azure.Tests
+
+    open NUnit.Framework
+
+    open Nessos.MBrace.Store
+    open Nessos.MBrace.Store.Tests
+
+    [<TestFixture; Category("CustomStores")>]
+    type ``WindowsAzure tests`` () =
+        inherit ``Store tests`` ()
+
+        let conn = ConnectionsConfig.get "Azure" 
+        let factory = new Nessos.MBrace.Azure.AzureStoreFactory() :> ICloudStoreFactory
+        let store = factory.CreateStoreFromConnectionString(conn)
+
+        override test.Store = store
--- tests/MBrace.Store.Tests/StoreTests.fs
+++ tests/MBrace.Store.Tests/StoreTests.fs
@@ -323,16 +323,6 @@
         override test.Store = store
 
     [<TestFixture; Category("CustomStores")>]
-    type ``WindowsAzure tests`` () =
-        inherit ``Store tests`` ()
-
-        let conn = ConnectionsConfig.get "Azure" 
-        let factory = new Nessos.MBrace.Azure.AzureStoreFactory() :> ICloudStoreFactory
-        let store = factory.CreateStoreFromConnectionString(conn)
-
-        override test.Store = store
-
-    [<TestFixture; Category("CustomStores")>]
     type ``SqlServer tests`` () =
         inherit ``Store tests`` ()
 
